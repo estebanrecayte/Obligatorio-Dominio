db4aea8fcd881a4e332cdecf18f9b4517c2c6684
mateorodriguez <MR192391@fi365.ort.edu.uy>
2023-09-03 12:57:30 -0300

    se agrega comentario y se fixean errores

--- Obligatorio Program/Program.cs
+++ Obligatorio Program/Program.cs
@@ -1,73 +1,10 @@
-<<<<<<< HEAD
-﻿using System;
+using System;
 
-class Program
+namespace Obligatorio_Program
 {
-    static void Main(string[] args)
-=======
-﻿using Obligatorio_Dominio;
-namespace Obligatorio_Program;
-    internal class Program
->>>>>>> 0a0bc9192dd8304bf3e84b1ab9b1c70462f994e6
+    class Program
     {
-        int opcion;
-        do
-        {
-<<<<<<< HEAD
-            Console.Clear();
-            Console.WriteLine("Menú de la Red Social\n");
-            Console.WriteLine("1 - Alta de miembro");
-            Console.WriteLine("2 - Listar publicaciones por email de miembro");
-            Console.WriteLine("3 - Listar posts en los que ha realizado comentarios por email de miembro");
-            Console.WriteLine("4 - Listar posts entre dos fechas");
-            Console.WriteLine("5 - Obtener miembros con más publicaciones");
-            Console.WriteLine("0 - Salir");
-
-            opcion = PedirNumero();
-
-            switch (opcion)
-            {
-                case 1:
-                    // Implementar meto2
-                    break;
-                case 2:
-                    // Implementar meto2
-                    break;
-                case 3:
-                    // Implementar meto2
-                    break;
-                case 4:
-                    // Implementar meto2
-                    break;
-                case 5:
-                    // Implementar meto2
-                    break;
-            }
-        } while (opcion != 0);
-    }
-
-    static int PedirNumero()
-    {
-        int numero;
-        string numeroIngresado;
-
-        while (true)
-        {
-            numeroIngresado = Console.ReadLine();
-            try
-            {
-                numero = int.Parse(numeroIngresado);
-                return numero;
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Ingrese un número válido - PRUEBA.");
-            }
-        }
-    }
-
-}
-=======
+        static void Main(string[] args)
         {
             int opcion;
             do
@@ -86,21 +23,31 @@ namespace Obligatorio_Program;
                 switch (opcion)
                 {
                     case 1:
-                        // Implementar meto2
+                        // Implementar alta de miembro
                         break;
                     case 2:
-                        // Implementar meto2
+                        // Implementar listar publicaciones por email de miembro
                         break;
                     case 3:
-                        // Implementar meto2
+                        // Implementar listar posts en los que ha realizado comentarios por email de miembro
                         break;
                     case 4:
-                        // Implementar meto2
+                        // Implementar listar posts entre dos fechas
                         break;
                     case 5:
-                        // Implementar meto2
+                        // Implementar obtener miembros con más publicaciones
+                        break;
+                    case 0:
+                        Console.WriteLine("Saliendo del programa.");
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida. Intente nuevamente.");
                         break;
                 }
+
+                Console.WriteLine("Presione cualquier tecla para continuar...");
+                Console.ReadKey();
+
             } while (opcion != 0);
         }
 
@@ -117,12 +64,12 @@ namespace Obligatorio_Program;
                     numero = int.Parse(numeroIngresado);
                     return numero;
                 }
-                catch (Exception)
+                catch (FormatException)
                 {
                     Console.WriteLine("Ingrese un número válido.");
+                    //commit para arreglar errores
                 }
             }
         }
-        }
     }
->>>>>>> 0a0bc9192dd8304bf3e84b1ab9b1c70462f994e6
+}
