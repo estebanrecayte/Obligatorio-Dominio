ef0da763a669cb1ee93997c6804daea54dc1d989
Esteban <estebanrecayte@gmail.com>
2023-09-07 21:10:53 -0300

    Agregando clase Invitacion y Miembro

---
+++ Obligatorio Dominio/Invitacion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obligatorio_Dominio
+{
+    public class Invitacion
+    {
+        public int Id {get; set;}
+        public Miembro MiembroSolicito { get; set;}
+        public Miembro MiembroSolicitante { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public Estado Estado { get; set; }
+
+
+        public Invitacion(int id, Miembro miembroSolicito, Miembro miembroSolicitante, DateTime fecha, Estado estado) {
+
+            Id = id;
+            MiembroSolicito = miembroSolicito;
+            MiembroSolicitante = miembroSolicitante;
+            Fecha = fecha;
+            Estado = estado;
+        }
+    }
+}
---
+++ Obligatorio Dominio/Miembro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obligatorio_Dominio
+{
+    public class Miembro
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public bool Bloqueado { get; set; }
+        public List<Miembro> ListaAmigos { get; set; }
+
+        public Miembro(int id,string nombre,string apellido,DateTime fechaNacimiento, bool bloqueado, List<Miembro> listaAmigos)
+        {
+            Id = id;
+            Nombre = nombre;
+            Apellido = apellido;
+            FechaNacimiento = fechaNacimiento;
+            Bloqueado = bloqueado;
+            ListaAmigos = listaAmigos;
+        }
+    }
+}
